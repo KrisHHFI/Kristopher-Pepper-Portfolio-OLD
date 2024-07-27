41cdba8c906adf8257cd6b66269f15b904cdeea9
KrisHHFI <kristopherpepperuk@gmail.com>
2024-07-27 18:36:55 +0300

    Carousel arrows now change the current BW image

--- src/FrontEnd/Components/ImageCarousel.js
+++ src/FrontEnd/Components/ImageCarousel.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BWPortraitImageSources, ComponentImageSources } from '../../BackEnd/ImageSources';
 import '../../App.css'; 
 
 const ImageCarousel = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const bwImages = Object.values(BWPortraitImageSources);
+
+  const handleNext = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % bwImages.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + bwImages.length) % bwImages.length);
+  };
+
   return (
     <div className="carouselContainer">
       <img
         src={ComponentImageSources.leftArrow}
         alt="Left Arrow"
         className="arrowImage leftArrowImage"
+        onClick={handlePrev}
       />
-      <img src={BWPortraitImageSources.bw1} alt="carousel" className="image" />
+      <img src={bwImages[currentImageIndex]} alt="carousel" className="image" />
       <img
         src={ComponentImageSources.rightArrow}
         alt="Right Arrow"
         className="arrowImage rightArrowImage"
+        onClick={handleNext}
       />
     </div>
   );
