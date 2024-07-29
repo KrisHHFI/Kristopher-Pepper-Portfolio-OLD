e60751aff77b6c6c96660a77130d7a4a1448c9a1
KrisHHFI <kristopherpepperuk@gmail.com>
2024-07-29 21:57:38 +0300

    Styling tweaks 2

--- src/App.js
+++ src/App.js
@@ -3,59 +3,10 @@ import React, { useState } from 'react';
 import './App.css';
 import PageContent from './FrontEnd/Components/PageContent';
 import Navbar from './FrontEnd/Components/Navbar';
-import { BandImageSources, BWPortraitImageSources, ColourPortraitSources } from './BackEnd/ImageSources';
+import pages from './Constants/Pages';
 
 function App() {
   const [activePage, setActivePage] = useState('BW Portraits');
-
-  const pages = {
-    'BW Portraits': {
-      title: 'BW Portraits',
-      hasCarousel: true,
-      imageSources: BWPortraitImageSources,
-      section: 'Photography',
-    },
-    'Colour Portraits': {
-      title: 'Colour Portraits',
-      hasCarousel: true,
-      imageSources: ColourPortraitSources,
-      section: 'Photography',
-    },
-    'Bands': {
-      title: 'Bands',
-      hasCarousel: true,
-      imageSources: BandImageSources,
-      section: 'Photography',
-    },
-    'Tic Tac Farm': {
-      title: 'Tic Tac Farm',
-      hasSplitLayout: true,
-      imageSources: BandImageSources,
-      section: 'Coding',
-    },
-    'React Weather App': {
-      title: 'React Weather App',
-      hasSplitLayout: true,
-      imageSources: BandImageSources,
-      section: 'Coding',
-    },
-    'Other Projects': {
-      title: 'Other Projects',
-      imageSources: BandImageSources,
-      section: 'Coding',
-    },
-    'Me': {
-      title: 'Me',
-      imageSources: BandImageSources,
-      section: 'About',
-    },
-    'Contact': {
-      title: 'Contact',
-      imageSources: BandImageSources,
-      section: 'About',
-    },
-  };
-
   const { title, hasCarousel = false, hasSplitLayout = false, imageSources } = pages[activePage];
 
   return (
---
+++ src/Constants/Pages.js
@@ -0,0 +1,57 @@
+import { BandImageSources, BWPortraitImageSources, ColourPortraitSources } from '../BackEnd/ImageSources';
+
+const pages = {
+  'BW Portraits': {
+    title: 'BW Portraits',
+    hasCarousel: true,
+    imageSources: BWPortraitImageSources,
+    section: 'Photography',
+  },
+  'Colour Portraits': {
+    title: 'Colour Portraits',
+    hasCarousel: true,
+    imageSources: ColourPortraitSources,
+    section: 'Photography',
+  },
+  'Bands': {
+    title: 'Bands',
+    hasCarousel: true,
+    imageSources: BandImageSources,
+    section: 'Photography',
+  },
+  'Tic Tac Farm': {
+    title: 'Tic Tac Farm',
+    hasSplitLayout: true,
+    imageSources: BandImageSources,
+    section: 'Coding',
+  },
+  'This Site': {
+    title: 'This Site',
+    hasSplitLayout: true,
+    imageSources: BandImageSources,
+    section: 'Coding',
+  },
+  'React Weather App': {
+    title: 'React Weather App',
+    hasSplitLayout: true,
+    imageSources: BandImageSources,
+    section: 'Coding',
+  },
+  'Other Projects': {
+    title: 'Other Projects',
+    imageSources: BandImageSources,
+    section: 'Coding',
+  },
+  'Me': {
+    title: 'Me',
+    imageSources: BandImageSources,
+    section: 'About',
+  },
+  'Contact': {
+    title: 'Contact',
+    imageSources: BandImageSources,
+    section: 'About',
+  },
+};
+
+export default pages;
